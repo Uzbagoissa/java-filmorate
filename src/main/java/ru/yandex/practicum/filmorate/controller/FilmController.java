41b78b7b7926bd2ed448b668300a159058cc6348
Stas <stas.vk88@yandex.ru>
2022-09-26 22:26:25 +0200

    3

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.yandex.practicum.filmorate.exceptions.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.HashSet;
 @RequestMapping("/films")
 public class FilmController {
     private final HashSet<Film> films = new HashSet<>();
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
 
     @GetMapping()
     public HashSet<Film> getFilms() {
@@ -20,17 +23,23 @@ public class FilmController {
     @PostMapping()
     public Film addFilm(@RequestBody Film film) throws FilmAlreadyExistException, InvalidFilmNameException, InvalidDescriptionException, InvalidReleaseDateException, InvalidDurationException {
         if (films.contains(film)){
+            log.error("Фильм уже был добавлен!, {}", film);
             throw new FilmAlreadyExistException("Фильм уже был добавлен!");
         } else if (film.getName().trim().equals("")){
+            log.error("Название не может быть пустым!, {}", film);
             throw new InvalidFilmNameException("Название не может быть пустым!");
         } else if (film.getDescription().length() > 200){
+            log.error("Максимальная длина описания — 200 символов!, {}", film);
             throw new InvalidDescriptionException("Максимальная длина описания — 200 символов!");
         } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
+            log.error("Дата релиза не должна быть раньше 28 декабря 1895!, {}", film);
             throw new InvalidReleaseDateException("Дата релиза не должна быть раньше 28 декабря 1895!");
         } else if (film.getDuration() <= 0){
+            log.error("Продолжительность фильма должна быть положительна!, {}", film);
             throw new InvalidDurationException("Продолжительность фильма должна быть положительна!");
         } else {
             films.add(film);
+            log.info("Добавлен новый фильм, {}", film);
             return film;
         }
     }
@@ -38,16 +47,25 @@ public class FilmController {
     @PutMapping()
     public Film renewFilm(@RequestBody Film film) throws InvalidFilmNameException, InvalidDescriptionException, InvalidReleaseDateException, InvalidDurationException {
         if (film.getName().trim().equals("")){
+            log.error("Название не может быть пустым!, {}", film);
             throw new InvalidFilmNameException("Название не может быть пустым!");
         } else if (film.getDescription().length() > 200){
+            log.error("Максимальная длина описания — 200 символов!, {}", film);
             throw new InvalidDescriptionException("Максимальная длина описания — 200 символов!");
         } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
+            log.error("Дата релиза не должна быть раньше 28 декабря 1895!, {}", film);
             throw new InvalidReleaseDateException("Дата релиза не должна быть раньше 28 декабря 1895!");
         } else if (film.getDuration() <= 0){
+            log.error("Продолжительность фильма должна быть положительна!, {}", film);
             throw new InvalidDurationException("Продолжительность фильма должна быть положительна!");
-        } else {
+        } else if(films.contains(film)){
             films.remove(film);
             films.add(film);
+            log.info("Фильм обновлен - , {}", film);
+            return film;
+        } else {
+            films.add(film);
+            log.info("Добавлен новый фильм, {}", film);
             return film;
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.InvalidBirthdayException;
 import ru.yandex.practicum.filmorate.exceptions.InvalidEmailException;
@@ -14,6 +16,7 @@ import java.util.HashSet;
 @RequestMapping("/users")
 public class UserController {
     private final HashSet<User> users = new HashSet<>();
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping()
     public HashSet<User> getUsers() {
@@ -23,19 +26,25 @@ public class UserController {
     @PostMapping()
     public User createUser(@RequestBody User user) throws UserAlreadyExistException, InvalidEmailException, InvalidLoginException, InvalidBirthdayException {
         if (users.contains(user)){
+            log.error("Такой пользователь уже существует!, {}", user);
             throw new UserAlreadyExistException("Такой пользователь уже существует!");
         } else if (user.getEmail().trim().equals("") || !user.getEmail().contains("@")){
+            log.error("Электронная почта не может быть пустой и должна содержать символ - @, {}", user);
             throw new InvalidEmailException("Электронная почта не может быть пустой и должна содержать символ - @");
         } else if (user.getLogin().equals("") || user.getLogin().contains(" ")){
+            log.error("Логин не может быть пустым и содержать пробелы!, {}", user);
             throw new InvalidLoginException("Логин не может быть пустым и содержать пробелы!");
         } else if (user.getBirthday().isAfter(LocalDate.now())){
+            log.error("Дата рождения не может быть в будущем!, {}", user);
             throw new InvalidBirthdayException("Дата рождения не может быть в будущем!");
         } else if (user.getName().trim().equals("")){
             user.setName(user.getLogin());
             users.add(user);
+            log.info("Добавлен новый пользователь, {}", user);
             return user;
         } else {
             users.add(user);
+            log.info("Добавлен новый пользователь, {}", user);
             return user;
         }
     }
@@ -43,10 +52,13 @@ public class UserController {
     @PutMapping()
     public User renewUser(@RequestBody User user) throws InvalidEmailException, InvalidLoginException, InvalidBirthdayException {
         if (user.getEmail().trim().equals("") || !user.getEmail().contains("@")){
+            log.error("Электронная почта не может быть пустой и должна содержать символ - @, {}", user);
             throw new InvalidEmailException("Электронная почта не может быть пустой и должна содержать символ - @");
         } else if (user.getLogin().equals("") || user.getLogin().contains(" ")){
+            log.error("Логин не может быть пустым и содержать пробелы!, {}", user);
             throw new InvalidLoginException("Логин не может быть пустым и содержать пробелы!");
         } else if (user.getBirthday().isAfter(LocalDate.now())){
+            log.error("Дата рождения не может быть в будущем!, {}", user);
             throw new InvalidBirthdayException("Дата рождения не может быть в будущем!");
         } else if(users.contains(user)) {
             users.remove(user);
@@ -54,12 +66,14 @@ public class UserController {
                 user.setName(user.getLogin());
             }
             users.add(user);
+            log.info("Пользователь обновлен - , {}", user);
             return user;
         } else {
             if (user.getName().trim().equals("")){
                 user.setName(user.getLogin());
             }
             users.add(user);
+            log.info("Добавлен новый пользователь, {}", user);
             return user;
         }
     }
